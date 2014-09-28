1e5650d2196a4f7f65ce820907b724e405812200
Georgi Yankov <georgi.st.yankov@gmail.com>
2014-09-28 20:00:22 +0300

    commit

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -6,13 +6,13 @@
 
 <div class="form-group<?php if ($errors->has('email')) echo ' has-error'; ?>">
     {{Form::label('email', 'Email', $attributes = array('class' => 'control-label') )}}
-    {{Form::email('email', $value = null, $attributes = array('placeholder' => 'Enter email', 'class' => 'form-control', 'required' => 'required'))}}
+    {{Form::email('email', $value = null, $attributes = array('placeholder' => 'Email', 'class' => 'form-control', 'required' => 'required'))}}
     @if ($errors->has('email')) <span class="error">{{$errors->first('email')}}</span> @endif
 </div>
 
 <div class="form-group<?php if ($errors->has('password')) echo ' has-error'; ?>">
     {{Form::label('password', 'Password', $attributes = array('class' => 'control-label') )}}
-    {{Form::password('password', $attributes = array('placeholder' => 'Enter password', 'class' => 'form-control', 'required' => 'required'))}}
+    {{Form::password('password', $attributes = array('placeholder' => 'Password', 'class' => 'form-control', 'required' => 'required'))}}
     @if ($errors->has('password')) <span class="error">{{$errors->first('password')}}</span> @endif
 </div>
 
@@ -22,7 +22,7 @@
     </label>
 </div>
 
-{{Form::submit('Login', $attributes = array('class' => 'btn btn-default'))}}
+{{Form::submit('Login', $attributes = array('class' => 'btn btn-primary'))}}
 
 {{ Form::close() }}
 
--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -6,29 +6,29 @@
 
 <div class="form-group<?php if ($errors->has('username')) echo ' has-error'; ?>">
     {{Form::label('username', 'Username', $attributes = array('class' => 'control-label') )}}
-    {{Form::text('username', $value = null, $attributes = array('placeholder' => 'Enter username', 'class' => 'form-control', 'required' => 'required'))}}
+    {{Form::text('username', $value = null, $attributes = array('placeholder' => 'Username', 'class' => 'form-control', 'required' => 'required'))}}
     {{$errors->first('username', '<span class="error">:message</span>')}}
 </div>
 
 <div class="form-group<?php if ($errors->has('email')) echo ' has-error'; ?>">
     {{Form::label('email', 'Email', $attributes = array('class' => 'control-label') )}}
-    {{Form::email('email', $value = null, $attributes = array('placeholder' => 'Enter email', 'class' => 'form-control', 'required' => 'required'))}}
+    {{Form::email('email', $value = null, $attributes = array('placeholder' => 'Email', 'class' => 'form-control', 'required' => 'required'))}}
     {{$errors->first('email', '<span class="error">:message</span>')}}
 </div>
 
 <div class="form-group<?php if ($errors->has('password')) echo ' has-error'; ?>">
     {{Form::label('password', 'Password', $attributes = array('class' => 'control-label') )}}
-    {{Form::password('password', $attributes = array('placeholder' => 'Enter password', 'class' => 'form-control', 'required' => 'required'))}}
+    {{Form::password('password', $attributes = array('placeholder' => 'Password', 'class' => 'form-control', 'required' => 'required'))}}
     {{$errors->first('password', '<span class="error">:message</span>')}}
 </div>
 
 <div class="form-group<?php if ($errors->has('repeat-password')) echo ' has-error'; ?>">
-    {{Form::label('repeat-password', 'Repeat password', $attributes = array('class' => 'control-label') )}}
-    {{Form::password('repeat-password', $attributes = array('placeholder' => 'Repeat password', 'class' => 'form-control', 'required' => 'required'))}}
+    {{Form::label('repeat-password', 'Repeat Password', $attributes = array('class' => 'control-label') )}}
+    {{Form::password('repeat-password', $attributes = array('placeholder' => 'Repeat Password', 'class' => 'form-control', 'required' => 'required'))}}
     {{$errors->first('repeat-password', '<span class="error">:message</span>')}}
 </div>
 
-{{Form::submit('Register', $attributes = array('class' => 'btn btn-default'))}}
+{{Form::submit('Register', $attributes = array('class' => 'btn btn-primary'))}}
 
 {{ Form::close() }}
 
